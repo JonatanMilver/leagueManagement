0e6405693499ac8eb3243319df91f2579114910d
JonatanMilver <48964784+JonatanMilver@users.noreply.github.com>
2021-06-04 13:44:04 +0300

    added assRepUtils with empty functions

---
+++ project/DataAccess/associationRepresentativeUtils.js
@@ -0,0 +1,13 @@
+const db_utils =  require('./DButils');
+
+async function checkGamePolicy(){
+
+}
+
+async function insertReferee(userId){
+    // TODO - Tomer
+}
+
+async function setRefereeInSeason(userId, seasonId){
+    // TODO - Tomer
+}
