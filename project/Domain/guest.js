e12bee33bf5f1e5132dc211df987a2ccf8d05ddd
tomerkel <73695609+tomerkel@users.noreply.github.com>
2021-06-04 13:33:50 +0300

    assoRepr structure

---
+++ project/Domain/guest.js
@@ -0,0 +1,13 @@
+
+
+function register(){
+    // register a guest to the system and create a new user for him.
+}
+
+function search(){
+    // guest can search players or teams in the system.
+}
+
+function sortBy(){
+    // guest can sort the results by ASC or DESC
+}
