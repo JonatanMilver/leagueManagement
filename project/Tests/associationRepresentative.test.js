c44487a93cb6cd9fb4c5fa0d9ae2dc88ac44de40
Guyzaidman <guyzaidm@gmail.com>
2021-06-07 16:17:27 +0300

    added ref tests

---
+++ project/Tests/associationRepresentative.test.js
@@ -0,0 +1,210 @@
+const request = require('supertest');
+const app = require('../main');
+require("dotenv").config();
+// var farUser = request.agent('http://localhost:3000');
+var farUser = request.agent(app);
+
+jest.setTimeout(30000);
+
+describe('/associationrepresentative - middleware', function() {
+    describe('User is not logged in', function() {
+      
+        test("Add game attempt Unsuccessful", async () => {
+            // let response = await request(app).post("/associationrepresentative/addGame").send({
+            response = await farUser.post("/associationrepresentative/addGame").send({
+                homeTeam: '',
+                awayTeam : '',
+                gameDateTime : '',
+                field : '',
+                refereeId : '',
+                leagueId : '',
+                seasonId : ''
+            });
+            expect(response.statusCode).toBe(401);
+            expect(response.text).toBe("You are not logged in");
+        })
+
+        test("Set game policy attempt Unsuccessful", async () => {
+            // let response = await request(app).post("/associationrepresentative/addGame").send({
+            response = await farUser.post("/associationrepresentative/setGamePolicy").send({
+                seasonId: '',
+                leagueId : '',
+                gamePolicyId : '',
+            });
+            expect(response.statusCode).toBe(401);
+            expect(response.text).toBe("You are not logged in");
+        })
+    })
+    describe('User does not have association rep permissions', function() {
+        
+        test("login - unpermited user", async () => {
+            response = await farUser.post("/Login").send({
+                username: 'guyzaid',
+                password: 'guyzaid'
+            });
+            expect(response.statusCode).toBe(200);
+        })
+
+        test("Add game attempt Unsuccessful", async () => {
+            response = await farUser.post("/associationrepresentative/addGame").send({
+                homeTeam: '',
+                awayTeam : '',
+                gameDateTime : '',
+                field : '',
+                refereeId : '',
+                leagueId : '',
+                seasonId : ''
+            });
+            expect(response.statusCode).toBe(401);
+            expect(response.text).toBe("You don't have the right permissions");
+        })
+    })
+})
+
+// const dummyGames = [
+//     {
+//         homeTeam : 10,
+//         awayTeam : 11,
+//         gameDateTime : "02/03/2021 19:30",
+//         field : "BS",
+//         refereeId : 1,
+//         seasonId : 1,
+//         leagueId : 1
+//     },
+//     {
+//         homeTeam : 11,
+//         awayTeam : 12,
+//         gameDateTime : "02/03/2021 19:30",
+//         field : "BS",
+//         refereeId : 1,
+//         seasonId : 1,
+//         leagueId : 1
+//     }
+// ]
+
+describe('POST /associationrepresentative/addGame', function() {
+    describe('Permissions', function(){    
+        describe('With permissions', function() {
+            // need to add permissions to galagas!!!
+            test("login - permited user", async () => {
+                response = await farUser.post("/Login").send({
+                    username: 'galagas',
+                    password: 'galagas'
+                });
+                expect(response.statusCode).toBe(200);
+            })
+    
+            test("Add game attempt Successful", async () => {
+                response = await farUser.post("/associationrepresentative/addGame").send({
+                    homeTeam : 10,
+                    awayTeam : 11,
+                    gameDateTime : "02/03/2021 19:30",
+                    field : "BS",
+                    refereeId : 1,
+                    seasonId : 1,
+                    leagueId : 1
+                });
+                expect(response.statusCode).toBe(200);
+                expect(response.text).toBe("Game has been succefully added.");
+            })
+        })
+    })
+    describe('Policy', function(){
+        describe('Without policy', function(){
+            test("login - permited user", async () => {
+                response = await farUser.post("/Login").send({
+                    username: 'galagas',
+                    password: 'galagas'
+                });
+                expect(response.statusCode).toBe(200);
+            })
+
+            test("Add game attempt unsuccessful - policy issue", async () => {
+                response = await request(app).post("/associationrepresentative/addGame").send({
+                    homeTeam: '',
+                    awayTeam : '',
+                    gameDateTime : '',
+                    field : '',
+                    refereeId : '',
+                    leagueId : '',
+                    seasonId : ''
+                });
+                expect(response.statusCode).toBe(409);
+                expect(response.body.message).toBe("No game policy for this season or game doesn't stand by policy.");
+            })
+        })
+        describe('With policy', function(){
+            test("login - permited user", async () => {
+                response = await farUser.post("/Login").send({
+                    username: 'galagas',
+                    password: 'galagas'
+                });
+                expect(response.statusCode).toBe(200);
+            })
+
+            test("Add game attempt successful - no policy issue", async () => {
+                response = await request(app).post("/associationrepresentative/addGame").send({
+                    homeTeam: '',
+                    awayTeam : '',
+                    gameDateTime : '',
+                    field : '',
+                    refereeId : '',
+                    leagueId : '',
+                    seasonId : ''
+                });
+                expect(response.statusCode).toBe(200);
+                expect(response.text).toBe("Game has been succefully added.");
+            })
+        })
+    })
+    describe('Arguments', function(){
+        describe('Missing arguments', function(){
+            test("login - permited user", async () => {
+                response = await farUser.post("/Login").send({
+                    username: 'galagas',
+                    password: 'galagas'
+                });
+                expect(response.statusCode).toBe(200);
+            })
+    
+            test("Add game attempt Unsuccessful", async () => {
+                response = await farUser.post("/associationrepresentative/addGame").send({
+                    homeTeam: '',
+                    // awayTeam : '',
+                    gameDateTime : '',
+                    field : '',
+                    refereeId : '',
+                    leagueId : '',
+                    seasonId : ''
+                });
+                expect(response.statusCode).toBe(406);
+                expect(response.text).toBe("Missing arguments.");
+            })
+        })
+    })
+});
+
+describe('/associationrepresentative/setGamePolicy', function(){
+    describe('Arguments', function(){
+        describe('Missing arguments', function(){
+            test("login - permited user", async () => {
+                response = await farUser.post("/Login").send({
+                    username: 'galagas',
+                    password: 'galagas'
+                });
+                expect(response.statusCode).toBe(200);
+            })
+    
+            test("Set game policy attempt Unsuccessful", async () => {
+                response = await farUser.post("/associationrepresentative/setGamePolicy").send({
+                    seasonId:'',
+                    leagueId:'',
+                    gamePolicyId: ''
+                });
+                expect(response.statusCode).toBe(406);
+                expect(response.text).toBe("Missing arguments");
+            })
+        })
+    })
+})
+    
