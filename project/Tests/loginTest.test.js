a35583acc150e5e6f72a7c1e7e29790f9f6758d0
Guyzaidman <guyzaidm@gmail.com>
2021-06-09 16:32:34 +0300

    merged logintests and assrep tests

--- project/Tests/loginTest.test.js
+++ project/Tests/loginTest.test.js
@@ -1,40 +1,45 @@
-const request = require('supertest');
-const app = require('../main');
-require("dotenv").config();
+// const request = require('supertest');
+// const app = require('../main');
+// require("dotenv").config();
+// var farUser = request.agent(app);
 
 
-jest.setTimeout(30000);
+// jest.setTimeout(30000);
 
-describe('GET /test', function() {
-    test("test test test test", async () => {
-        response = await request(app).get("/test");
-        expect(response.statusCode).toBe(200);
-    })
-});
+// describe('GET /test', function() {
+//     test("test test test test", async () => {
+//         // response = await request(app).get("/test");
+//         response = await farUser.get("/test");
+//         expect(response.statusCode).toBe(200);
+//     })
+// });
 
-describe('POST /Login', function() {
-    test("Successful login", async () => {
-        response = await request(app).post("/Login").send({
-            username: 'guyzaid',
-            password: 'guyzaid'
-        });
-        expect(response.statusCode).toBe(200);
-    })
+// describe('POST /Login', function() {
+//     test("Successful login", async () => {
+//         // response = await request(app).post("/Login").send({
+//         response = await farUser.post("/Login").send({
+//             username: 'guyzaid',
+//             password: 'guyzaid'
+//         });
+//         expect(response.statusCode).toBe(200);
+//     })
 
-    test("Unsuccessful login - username does not exist", async () => {
-        response = await request(app).post("/Login").send({
-            username: 'guyaid',
-            password: 'guyzaid'
-        });
-        expect(response.statusCode).toBe(401);
-    })
+//     test("Unsuccessful login - username does not exist", async () => {
+//         // response = await request(app).post("/Login").send({
+//             response = await farUser.post("/Login").send({
+//             username: 'guyaid',
+//             password: 'guyzaid'
+//         });
+//         expect(response.statusCode).toBe(401);
+//     })
 
-    test("Unsuccessful login - missing parameters", async () => {
-        response = await request(app).post("/Login").send({
-            // username: 'guyzaid',
-            password: 'guyzaid'
-        });
-        expect(response.statusCode).toBe(409);
-        expect(response.text).toBe("Missing arguments");
-    })
-});
+//     test("Unsuccessful login - missing parameters", async () => {
+//         // response = await request(app).post("/Login").send({
+//             response = await farUser.post("/Login").send({
+//             // username: 'guyzaid',
+//             password: 'guyzaid'
+//         });
+//         expect(response.statusCode).toBe(409);
+//         expect(response.text).toBe("Missing arguments");
+//     })
+// });
